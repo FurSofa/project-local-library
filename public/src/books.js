19faff553014b2aa866b9ae15d6a7f2ae4bc029e
fursofa <cml6497@gmail.com>
2020-12-16 15:55:20 -0500

    src/books.js complete

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,36 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  return authors.find((author) => author.id === id);
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  return books.find((book) => book.id === id);
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+let partitionBooksByBorrowedStatus = (books) => {
+  let borrowedBooks = books.filter((book) => {
+    return book.borrows[0].returned === false;
+  });
+  let returnedBooks = books.filter((book) => {
+    return book.borrows[0].returned == true;
+  });
+  return [borrowedBooks, returnedBooks];
+};
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  let returnArray = [];
+  let borrows = book.borrows;
+  borrows.forEach((record) => {
+    for (const key in accounts) {
+      const account = accounts[key]; // account = obj
+      if (record.id === account.id) {
+        let copyAccount = { ...account };
+        copyAccount["returned"] = record.returned;
+        returnArray.push(copyAccount);
+      }
+    }
+  });
+  return returnArray.slice(0, 10);
+}
 
 module.exports = {
   findAuthorById,
--- test.js
+++ test.js
@@ -14,32 +14,81 @@ const account = {
   email: "ingram.garrison@syntac.net",
   registered: "Tuesday, February 13, 2018 9:11 PM",
 };
-
-let getBooksPossessedByAccount = (account, books, authors) => {
-  let ownedBooks = [];
-  for (let idx in books) {
-    let book = books[idx];
-    book.borrows.forEach((record) => {
-      if (account.id === record.id && record.returned === false) {
-        let bookCopy = book;
-        for (let i in authors) {
-          let author = authors[i];
-          if (author.id === book.authorId) {
-            bookCopy["author"] = author;
-            ownedBooks.push(bookCopy);
-          }
-        }
-      }
-    });
-  }
-  return ownedBooks;
+const book = {
+  id: "5f4471327864ee880caf5afc",
+  title: "reprehenderit quis laboris adipisicing et",
+  genre: "Poetry",
+  authorId: 20,
+  borrows: [
+    {
+      id: "5f446f2e2a4fcd687493a775",
+      returned: false,
+    },
+    {
+      id: "5f446f2ebe8314bcec531cc5",
+      returned: true,
+    },
+    {
+      id: "5f446f2ea508b6a99c3e42c6",
+      returned: true,
+    },
+    {
+      id: "5f446f2e5e2952040e9f9b88",
+      returned: true,
+    },
+    {
+      id: "5f446f2eae901a82e0259947",
+      returned: true,
+    },
+    {
+      id: "5f446f2ee5be00208a4481e0",
+      returned: true,
+    },
+    {
+      id: "5f446f2ebe8314bcec531cc5",
+      returned: true,
+    },
+    {
+      id: "5f446f2ee176f80b8d5d24da",
+      returned: true,
+    },
+    {
+      id: "5f446f2ed524723353040e5c",
+      returned: true,
+    },
+  ],
 };
-getBooksPossessedByAccount(account, books, authors);
-// console.log(getBooksPossessedByAccount(account, books, authors));
 
-
-
-function compareIdToAccountId(account){
-  if(account.id === id) 
-    return true
+function getBorrowersForBook1(book, accounts) {
+  let returnArray = [];
+  let borrows = book.borrows;
+  borrows.forEach((record) => {
+    for (const key in accounts) {
+      const account = accounts[key]; // account = obj
+      if (record.id === account.id) {
+        let copyAccount = { ...account };
+        copyAccount["returned"] = record.returned;
+        returnArray.push(copyAccount);
+      }
+    }
+  });
+  return returnArray.slice(0, 10);
 }
+let getBorrowersForBook = (book, accounts) => {
+  let result = [];
+  let borrows = book.borrows;
+  for (let record in borrows) {
+    for (let index in accounts) {
+      let account = accounts[index];
+      if (record.id === account.id) {
+        let tempAcct = account;
+        tempAcct["returned"] = record.returned;
+
+        result.push(tempAcct);
+      }
+    }
+  }
+  return result.slice(0, 10);
+};
+
+console.log(getBorrowersForBook(book, accounts));
