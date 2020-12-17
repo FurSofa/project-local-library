42c5c038a5a21ea07600fc33dcb954ab672ef528
fursofa <cml6497@gmail.com>
2020-12-16 20:07:38 -0500

    comment and commit home.js

--- public/src/home.js
+++ public/src/home.js
@@ -1,20 +1,110 @@
-function totalBooksCount(books) {}
+function sorter(arr) {
+  // Local helper function
+  // receives arr of obj
+  // sorts by obj.count and slice top 5
+  return arr
+    .sort((a, b) => {
+      if (a.count < b.count) return 1;
+      if (a.count > b.count) return -1;
+      return 0;
+    })
+    .slice(0, 5);
+}
+function getAuthorName(authors, id) {
+  // Local helper function
+  // receives author ID and authors array
+  // returns author first and last name as string
+  let authName = "";
+  authors.forEach((author) => {
+    if (author.id == id) authName = `${author.name.first} ${author.name.last}`;
+  });
+  return authName;
+}
 
-function totalAccountsCount(accounts) {}
+function totalBooksCount(books) {
+  //get total number of books
+  return books.length;
+}
 
-function booksBorrowedCount(books) {}
+function totalAccountsCount(accounts) {
+  //get total number of accounts
+  return accounts.length;
+}
 
-function mostCommonGenres(books) {}
+function booksBorrowedCount(books) {
+  //get total number of books checked out
+  let counter = 0;
+  books.forEach((book) => {
+    if (book.borrows[0].returned === false) counter += 1;
+  });
+  return counter;
+}
 
-function mostPopularBooks(books) {}
+function getMostCommonGenres(books) {
+  //get top 5 most common genres
+  let allGenres = books.map((book) => book.genre);
+  let allGenreObj = allGenres.reduce((acc, genre) => {
+    acc[genre] ? acc[genre]++ : (acc[genre] = 1);
+    return acc;
+  }, {});
+  let rtnArr = [];
+  for (const key in allGenreObj) {
+    const element = allGenreObj[key];
+    let newObj = {};
+    newObj["name"] = key;
+    newObj["count"] = element;
+    rtnArr.push(newObj);
+  }
+  let result = rtnArr.sort((a, b) => {
+    if (a.count < b.count) return 1;
+    if (a.count > b.count) return -1;
+    return 0;
+  });
+  console.log(result.slice(0, 5));
+  return result.slice(0, 5);
+}
 
-function mostPopularAuthors(books, authors) {}
+function getMostPopularBooks(books) {
+  // get most popular books
+  let result = [];
+  books.forEach((book) => {
+    let newObj = {};
+    newObj["name"] = book.title;
+    newObj["count"] = book.borrows.length;
+    result.push(newObj);
+  });
+  return sorter(result); // sort array by most popular and slice top 5
+}
+
+function getMostPopularAuthors(books, authors) {
+  //get top 5 most popular authors
+  //params: Books: array of obj
+  //params: authors: array of obj
+  //returns: arr of obj
+  let rtnArr = [];
+  let authorBookCountObj = books.reduce((acc, book) => {
+    // get author ID and book checkout counts in obj
+    acc[book.authorId]
+      ? (acc[book.authorId] += book.borrows.length)
+      : (acc[book.authorId] = book.borrows.length);
+    return acc;
+  }, {});
+  for (const key in authorBookCountObj) {
+    //create object for each author and put into return array
+    const value = authorBookCountObj[key];
+    let newObj = {};
+    newObj["name"] = getAuthorName(authors, parseInt(key));
+    newObj["count"] = value;
+    rtnArr.push(newObj);
+  }
+  return sorter(rtnArr); // sort array by most popular and slice top 5
+}
 
 module.exports = {
   totalBooksCount,
   totalAccountsCount,
   booksBorrowedCount,
-  mostCommonGenres,
-  mostPopularBooks,
-  mostPopularAuthors,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
 };
--- test.js
+++ test.js
@@ -1,94 +1,98 @@
-const accounts = require("./public/data/accounts");
-const authors = require("./public/data/authors");
-const books = require("./public/data/books");
-
-const account = {
-  id: "5f446f2ed92454081fb1999b",
-  picture: "https://api.adorable.io/avatars/75/ingram.garrison@syntac.net",
-  age: 29,
-  name: {
-    first: "Ingram",
-    last: "Garrison",
-  },
-  company: "SYNTAC",
-  email: "ingram.garrison@syntac.net",
-  registered: "Tuesday, February 13, 2018 9:11 PM",
-};
-const book = {
-  id: "5f4471327864ee880caf5afc",
-  title: "reprehenderit quis laboris adipisicing et",
-  genre: "Poetry",
-  authorId: 20,
-  borrows: [
-    {
-      id: "5f446f2e2a4fcd687493a775",
-      returned: false,
-    },
-    {
-      id: "5f446f2ebe8314bcec531cc5",
-      returned: true,
-    },
-    {
-      id: "5f446f2ea508b6a99c3e42c6",
-      returned: true,
-    },
-    {
-      id: "5f446f2e5e2952040e9f9b88",
-      returned: true,
-    },
-    {
-      id: "5f446f2eae901a82e0259947",
-      returned: true,
-    },
-    {
-      id: "5f446f2ee5be00208a4481e0",
-      returned: true,
-    },
-    {
-      id: "5f446f2ebe8314bcec531cc5",
-      returned: true,
-    },
-    {
-      id: "5f446f2ee176f80b8d5d24da",
-      returned: true,
-    },
-    {
-      id: "5f446f2ed524723353040e5c",
-      returned: true,
-    },
-  ],
-};
-
-function getBorrowersForBook1(book, accounts) {
-  let returnArray = [];
-  let borrows = book.borrows;
-  borrows.forEach((record) => {
-    for (const key in accounts) {
-      const account = accounts[key]; // account = obj
-      if (record.id === account.id) {
-        let copyAccount = { ...account };
-        copyAccount["returned"] = record.returned;
-        returnArray.push(copyAccount);
-      }
-    }
-  });
-  return returnArray.slice(0, 10);
+function totalBooksCount(books) {
+  return books.length
 }
-let getBorrowersForBook = (book, accounts) => {
-  let result = [];
-  let borrows = book.borrows;
-  for (let record in borrows) {
-    for (let index in accounts) {
-      let account = accounts[index];
-      if (record.id === account.id) {
-        let tempAcct = account;
-        tempAcct["returned"] = record.returned;
 
-        result.push(tempAcct);
-      }
-    }
+function totalAccountsCount(accounts) {
+  return accounts.length
+}
+
+function booksBorrowedCount(books) {
+  let counter = 0;
+  books.forEach((book) => {
+    if (book.borrows[0].returned === false) counter += 1;
+  });
+  return counter;
+}
+
+function getMostCommonGenres(books) {
+  let allGenres = books.map((book) => book.genre);
+  let allGenreObj = allGenres.reduce((acc, genre) => {
+    acc[genre] ? acc[genre]++ : (acc[genre] = 1);
+    return acc;
+  }, {});
+  let rtnArr = []
+  for (const key in allGenreObj) {
+      const element = allGenreObj[key];
+      let newObj = {}
+      newObj["name"] = key
+      newObj["count"] = element
+      rtnArr.push(newObj)
   }
-  return result.slice(0, 10);
-};
+  let result = rtnArr.sort((a,b)=> {
+    if (a.count < b.count) return 1
+    if (a.count > b.count) return -1
+    return 0
+  })
+  console.log(result.slice(0,5));
+  return result.slice(0,5)
+}
 
-console.log(getBorrowersForBook(book, accounts));
+function getMostPopularBooks (books) {
+
+  let result = [];
+  books.forEach((book) => {
+    let newObj = {};
+    newObj["name"] = book.title;
+    newObj["count"] = book.borrows.length;
+    result.push(newObj);
+  });
+  return sorter(result).slice(0,5)
+}
+
+function sorter(arr) {
+  // Local helper function
+  // receives arr of obj
+  // sorts by obj.count and slice top 5
+  return arr
+    .sort((a, b) => {
+      if (a.count < b.count) return 1;
+      if (a.count > b.count) return -1;
+      return 0;
+    })
+    .slice(0, 5);
+}
+function getAuthorName(authors, id) {
+  let authName = "";
+  authors.forEach((author) => {
+    if (author.id == id) authName = `${author.name.first} ${author.name.last}`;
+  });
+  return authName;
+}
+
+function getMostPopularAuthors(books, authors) {
+  let rtnArr = [];
+  let authorBookCountObj = books.reduce((acc, book) => {
+    acc[book.authorId]
+      ? (acc[book.authorId] += book.borrows.length)
+      : (acc[book.authorId] = book.borrows.length);
+    return acc;
+  }, {});
+  console.log(authorBookCountObj);
+  for (const key in authorBookCountObj) {
+    const value = authorBookCountObj[key];
+    let newObj = {};
+    newObj["name"] = getAuthorName(authors, parseInt(key));
+    newObj["count"] = value;
+    rtnArr.push(newObj);
+  }
+  return sorter(rtnArr);
+}
+
+module.exports = {
+  totalBooksCount,
+  totalAccountsCount,
+  booksBorrowedCount,
+  getMostCommonGenres,
+  getMostPopularBooks,
+  getMostPopularAuthors,
+};
