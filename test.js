cb263b4cf5269f35035a17d9146a2a0e4f3f1d4b
fursofa <cml6497@gmail.com>
2020-12-20 14:17:40 -0500

    test

--- test.js
+++ test.js
@@ -1,23 +1,16 @@
-// const accounts = require("./public/data/accounts");
-// const authors = require("./public/data/authors");
-// const books = require("./public/data/books");
+const accounts = require("./public/data/accounts");
+const authors = require("./public/data/authors");
+const books = require("./public/data/books");
 
-// const account = {
-//   id: "5f446f2ed92454081fb1999b",
-//   picture: "https://api.adorable.io/avatars/75/ingram.garrison@syntac.net",
-//   age: 29,
-//   name: {
-//     first: "Ingram",
-//     last: "Garrison",
-//   },
-//   company: "SYNTAC",
-//   email: "ingram.garrison@syntac.net",
-//   registered: "Tuesday, February 13, 2018 9:11 PM",
-// };
+let rnd = Math.random(0 - 25);
+const account = accounts[2];
+const author = authors[rnd];
+const book = books[rnd];
 
-// const partitionBooksByBorrowedStatus = (books) => [
-//   books.filter((book) => !book.borrows[0].returned),
-//   books.filter((book) => book.borrows[0].returned),
-// ];
 
-// partitionBooksByBorrowedStatus(books, "lkajnwrefiuq3bn4kltrjn324");
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
